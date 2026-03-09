5eb2df315d063c582b767dafb82b288de85fcc8e
Ashwin kumar yadav <ashwinyaduvanshi218@gmail.com>
2026-03-09 21:31:48 +0530

    Time: 2 ms (19.45%) | Memory: 43.4 MB (88.83%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -23,3 +23,38 @@ class Solution {
         
     }
 }
+
+// optimal solution in O(1) and O(n)
+/* Dutch National Flag Algorithm
+
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while(mid <= high){
+
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }
+
+            else if(nums[mid] == 1){
+                mid++;
+            }
+
+            else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+        return ;
+    }
+}*/
