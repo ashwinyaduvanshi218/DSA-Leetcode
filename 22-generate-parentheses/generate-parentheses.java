1d2ca8bf6ece7990612b8dde79621588e687c2ef
Ashwin kumar yadav <ashwinyaduvanshi218@gmail.com>
2026-03-22 21:23:47 +0530

    Time: 1 ms (85.58%) | Memory: 44.6 MB (62.13%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -33,3 +33,32 @@ class Solution {
         }
     }
 }
+
+/*
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder("");
+        helper(n,n,ans,sb);
+        return ans;
+
+
+        
+    }
+    public void helper(int left , int right , List<String> ans , StringBuilder sb){
+        if(left ==0 && right ==0){
+            ans.add(sb.toString());
+            return;        
+        }
+        if(left>0){
+            sb.append('(');
+            helper(left - 1, right , ans ,sb);
+            sb.deleteCharAt(sb.length() -1);
+        }
+        if(right>left){
+            sb.append(')');
+            helper(left,right -1 , ans,sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}*/
