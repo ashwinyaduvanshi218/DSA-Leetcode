9b0d1a52f9f404d06520d5626738e6b7423c2893
Ashwin kumar yadav <ashwinyaduvanshi218@gmail.com>
2026-04-01 00:57:45 +0530

    Time: 4 ms (74.10%) | Memory: 45.3 MB (55.18%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -3,31 +3,31 @@ import java.util.*;
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> st = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer , Integer> map = new HashMap<>();
+        //4 1 2  1 3 4 2 
 
-        // Step 1: process nums2 from right to left
-        for (int i = nums2.length - 1; i >= 0; i--) {
+        for(int i = nums2.length-1 ; i >= 0 ; i--){
             int curr = nums2[i];
 
-            // pop all smaller elements
-            while (!st.isEmpty() && st.peek() <= curr) {
+            while(!st.isEmpty() && st.peek()<curr){
                 st.pop();
+
+
             }
+            int greatest = (st.isEmpty() ? -1 : st.peek());
+            map.put(curr , greatest); 
 
-            // store answer in map
-            int nextGreater = st.isEmpty() ? -1 : st.peek();
-            map.put(curr, nextGreater);
+            st.push(curr);  
 
-            // push current element
-            st.push(curr);
         }
-
-        // Step 2: build result for nums1
         int[] res = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
+        for(int i =0 ; i<nums1.length ; i++){
             res[i] = map.get(nums1[i]);
         }
+        return res ;
 
-        return res;
+
+       
+
     }
 }
