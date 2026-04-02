9470675922a3810ba775b56066fe9791b4eace0d
Ashwin kumar yadav <ashwinyaduvanshi218@gmail.com>
2026-04-03 00:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.49%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.java
+++ 556-next-greater-element-iii/next-greater-element-iii.java
@@ -2,43 +2,58 @@ class Solution {
     public int nextGreaterElement(int n) {
         char[] arr = String.valueOf(n).toCharArray();
         int len = arr.length;
+        
 
         // 1️⃣ Find breaking point (first decreasing element from right)
-        int i = len - 2;
-        while (i >= 0 && arr[i] >= arr[i + 1]) {
-            i--;
+        int i = len -2 ; 
+        while(i>=0 && arr[i]>=arr[i +1]){
+            i --;
         }
+         
+        
 
         // If no breaking point → no greater number possible
-        if (i < 0) return -1;
+        if(i<0) return -1 ;
+        
 
         // 2️⃣ Find just greater element from right
-        int j = len - 1;
-        while (arr[j] <= arr[i]) {
+        int j = len - 1; 
+        while(arr[i]>=arr[j]){
             j--;
         }
+        
 
         // 3️⃣ Swap
         char temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-
+        arr[i]= arr[j];
+        arr[j]=temp ;
+        
         // 4️⃣ Reverse the suffix (i+1 to end)
-        reverse(arr, i + 1, len - 1);
+        reverse(arr , i+1 , len -1 );
+        
 
         // 5️⃣ Convert back to number and check overflow
         long num = Long.parseLong(new String(arr));
-        return (num > Integer.MAX_VALUE) ? -1 : (int) num;
-    }
+        int newNum1 = (num>Integer.MAX_VALUE ) ? -1 : (int) num ;
+        return newNum1;
 
-    // Helper function to reverse array
-    private void reverse(char[] arr, int left, int right) {
-        while (left < right) {
-            char temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
+
+        
+
+        // Helper function to reverse array
+
+        
+    
+    }
+    private void reverse(char[] arr , int left , int right ){
+            while(left<right){
+            char temp = arr[left ];
+            arr[left]= arr[right];
+            arr[right]= temp;
             left++;
             right--;
+            }
+
         }
-    }
+    
 }
