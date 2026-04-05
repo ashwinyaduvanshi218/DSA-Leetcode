9f9c87828a0b5ab1d896b02ae2fc20c5af1e23dc
Ashwin kumar yadav <ashwinyaduvanshi218@gmail.com>
2026-04-05 22:11:12 +0530

    Time: 1 ms (63.68%) | Memory: 42.8 MB (96.92%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -20,3 +20,23 @@ class Solution {
         return first.substring(0, i);
     }
 }
+/*
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            // shrink prefix until it matches current string
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+}*/
